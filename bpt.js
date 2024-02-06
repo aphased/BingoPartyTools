168ba8b5e7702d5fd8ac9fb48fb1f195d959ca2b
aphased <152172867+aphased@users.noreply.github.com>
2024-02-06 16:17:28 +0100

    Added a bunch of new commands
    Also unified and refactored some existing code
    
    This commit also makes the moderator list/"allowed IGNs" public

--- bpt.js
+++ bpt.js
@@ -1,14 +1,7 @@
-import { BingoPartyTools } from './settings';
+import { BingoPartyTools } from "./settings";
 import { removeRank } from "./utils";
-import { allowlist, bingoBrewersRules } from './data';
+import { allowlist, bingoBrewersRules, partyHostNameWithoutRank } from "./data";
 
-
-/**
- * The current (2024-01) expected party leader's in-game account
- * name for probably at least the next few Bingo events – not strictly a
- * necessary requirement, but used for nicer-looking output formatting
- */
-partyHostNameWithoutRank = "BingoParty";
 
 /**
  * Checks if the passed in-game name is allowed to moderate the party.
@@ -25,6 +18,29 @@ function playerHasPermissions(formattedPlayerName) {
 }
 
 /**
+ * These exist for at least partially circumventing Hypixel's
+ * "You cannot say the same message twice!"
+ */
+let helpOutputIndex = 0;
+const helpMessages = ["r For a list of available commands see github dot com/aphased/bingopartytools", "r GitHub: aphased/bingopartytools for all commands",
+"r All commands are shown on GitHub: aphased/bingopartytools"];
+
+/**
+ * This function masks the true implementation of how messages/commands are
+ * output to Hypixel servers in the game, which _should_ make switching down
+ * the line from ChatTriggers to e.g. Mineflayer far more easy.
+ * @param {String} command  The command to be sent to Hypixel, **without**
+ * preceding slash. This is also applicable for regular chat messages, which
+ * should always specify their respective channel (e.g. `/pc <message to party>`, `/msg <ign> <direct message whisper>`, etc.)
+ */
+function outputCommand(command) {
+  // Using ChatTriggers command function, which prepends the slash for us:
+  ChatLib.command(command);
+  // Using Mineflayer later perhaps could look like:
+  // bot.chat("/" + command);
+}
+
+/**
  * Exists to turn a common three-line operation into a one-liner.
  * @param {String} command  Command message to be executed **without** leading
  * slash /
@@ -32,9 +48,11 @@ function playerHasPermissions(formattedPlayerName) {
  * milliseconds (thanks Hypixel)
  */
 function waitAndOutputCommand(command, timeout) {
+  // Ensure to wait at least half a second if this function is called
+  let waitDuration = timeout > 500 ? timeout : 500;
   setTimeout(() => {
-    ChatLib.command(command);
-  }, timeout);
+    outputCommand(command);
+  }, waitDuration);
   return;
 }
 
@@ -49,8 +67,8 @@ function waitAndOutputCommand(command, timeout) {
  */
 function checkSetting(settingCategory, setting, command) {
   if (!BingoPartyTools.getSetting(settingCategory, setting)) {
-    informDisabledSetting = "r This setting is currently disabled. ("+ command + ")";
-    ChatLib.command(informDisabledSetting);
+    let informDisabledSetting = "r This setting is currently disabled. ("+ command + ")";
+    outputCommand(informDisabledSetting);
     return -1;
   } else return command.charCodeAt(0); // some nonnegative number
 }
@@ -80,13 +98,12 @@ You have 60 seconds to accept. Click here to join!
 register("chat", function autoAcceptPartyInvite(formattedSenderName) {
   const unformattedPlayerName = removeRank(formattedSenderName).toLowerCase();
   if (!BingoPartyTools.getSetting("BingoPartyFeatures", "Party autoaccept")) {
-    //ChatLib.command("msg " + unformattedPlayerName + " This setting is currently disabled. ("+ "autoaccept party invite" + ")");
+    //outputCommand("msg " + unformattedPlayerName + " This setting is currently disabled. ("+ "autoaccept party invite" + ")");
     return;
   }
   if (!playerHasPermissions(formattedSenderName)) return;
   waitAndOutputCommand("party accept " + unformattedPlayerName, 1000);
 }).setChatCriteria("-----------------------------------------------------\n${name} has invited you to join ").setParameter("start");
-
 
 /**
  * The current main ChatTrigger: Party moderation commands sent to the host
@@ -101,7 +118,7 @@ register("chat", function processCommandFromDirectMessage(formattedSenderName, m
     return;
   }
 
-  messageWords = message.split(" ");
+  let messageWords = message.split(" ");
 
   var command = "";
   if (messageWords.length > 1) {
@@ -116,7 +133,7 @@ register("chat", function processCommandFromDirectMessage(formattedSenderName, m
     /* The messages are pretty soon blocked by Hypixel for repeating multiple
     times anyways… so for the time being at least, disallowed users are just
     not informed about their missing permissions at all. */
-    // ChatLib.command("r You do not have the permissions to run this! ("+ command + ")");
+    // outputCommand("r You do not have the permissions to run this! ("+ command + ")");
     return;
   }
 
@@ -144,11 +161,10 @@ register("chat", function processCommandFromDirectMessage(formattedSenderName, m
  */
 /*
 register("chat", function processCommandFromPartyChat(formattedSenderName, message) {
-  // Parse message…
+  // Parse message, then…
   // executeHypixelPartyCommand(formattedSenderName, command, commandArgument, message);
 }).setChatCriteria("Party > ${name}: ${message}");
 */
-
 
 /**
  * The function to be called when a new ChatTrigger is added, e.g. commands
@@ -165,7 +181,9 @@ register("chat", function processCommandFromPartyChat(formattedSenderName, messa
  */
 function executeHypixelPartyCommand(formattedSenderName, command, commandArgument, message) {
   // used for disband, help and default (invalid) commands in switch case below
-  casePreservingRankRemovedIGN = removeRank(formattedSenderName);
+  const casePreservingRankRemovedIGN = removeRank(formattedSenderName);
+
+  let receivingPlayerName = "";
 
   /* Hypixel quote 2024-01: "You are sending commands too fast! Please slow
   down." + "You can only send a message once every half second!"
@@ -173,23 +191,32 @@ function executeHypixelPartyCommand(formattedSenderName, command, commandArgumen
   Furthermore of note: ooffyy said 2024-01-07 that queueing commands was
   apparently disallowed by Hypixel, so we apply the wait individually every
   time instead of just using queue-like push/wait+send/pop operations */
-  defaultTimeoutMillis = 2190 + Math.floor(Math.random() * 41) - 20;
+  let defaultTimeoutMillis = 2190 + Math.floor(Math.random() * 41) - 20;
+
+  // remove the first two words – "!p command" – from the message string,
+  // where command := {speak,say,cmd}
+  // TODO: keep the number of words "sliced off" UPDATED in case of
+  // ever moving away from `!p command` system, to e.g. just `!command`
+  const messageToBroadcast = message.split(" ").slice(2).join(" ");
 
   /* Currently active commands (for full explanation, all aliases &
   usage see GitHub repository's README):
-  transfer, mute, promote, kick, block, unblock, stream, invite,
+  transfer, mute, promote, kick, kickoffline, block, unblock, stream, invite,
   allinvite (setting), speak, rule, help */
   switch(command) {
   case "disband":
     // disband is never allowed! "undocumented" non-command
     // (so we perform zero actions except for this funny-eerie bot response)
-    waitAndOutputCommand("r What exactly are your plans, " + casePreservingRankRemovedIGN +"? :raisedEyebrow:", defaultTimeoutMillis);
+    waitAndOutputCommand("r What exactly are your plans, " + casePreservingRankRemovedIGN + "? :raisedEyebrow:", defaultTimeoutMillis);
     break;
   case "transfer":
     if (checkSetting("BingoPartyFeatures", "Party transfer", command) < 0)
       return;
     receivingPlayerName = commandArgument;
-    ChatLib.command("pc Party was transferred to " + receivingPlayerName + " by " + formattedSenderName + ".");
+    if (receivingPlayerName === "") {
+      return;
+    }
+    outputCommand("pc Party was transferred to " + receivingPlayerName + " by " + formattedSenderName + ".");
     waitAndOutputCommand("p transfer " + receivingPlayerName, defaultTimeoutMillis);
     break;
   case "unmute":
@@ -197,7 +224,7 @@ function executeHypixelPartyCommand(formattedSenderName, command, commandArgumen
   case "mute":
     if (checkSetting("BingoPartyFeatures", "Party mute", command) < 0)
       return;
-    ChatLib.command("p mute");
+    outputCommand("p mute");
     waitAndOutputCommand("pc Party mute was used by " + formattedSenderName + ".", defaultTimeoutMillis);
     break;
   case "promote":
@@ -208,8 +235,13 @@ function executeHypixelPartyCommand(formattedSenderName, command, commandArgumen
       // no name supplied, thus promote command sender instead
       receivingPlayerName = removeRank(formattedSenderName).toLowerCase();
     }
-    ChatLib.command("pc " + receivingPlayerName + " was promoted by " + formattedSenderName + ".");
+    outputCommand("pc " + receivingPlayerName + " was promoted by " + formattedSenderName + ".");
     waitAndOutputCommand("p promote " + receivingPlayerName, defaultTimeoutMillis);
+    break;
+  case "kickafk":
+    // fallthrough for additional alias
+  case "kickoffline":
+    outputCommand("p kickoffline");
     break;
   case "remove":
     // fallthrough for additional alias
@@ -221,25 +253,35 @@ function executeHypixelPartyCommand(formattedSenderName, command, commandArgumen
       // Can't kick myself if I'm party leader, have a little fun instead. Also,
       // I attempted to make it a little less hard-coded unto myself and more
       // interchangeable by at least using a variable for the party host name…
-      ChatLib.command("pc " + formattedSenderName + " tried kicking [MVP++] " + partyHostNameWithoutRank + " from the party. L bozo!");
+      outputCommand("pc " + formattedSenderName + " tried kicking [MVP++] " + partyHostNameWithoutRank + " from the party. L bozo!");
       return;
     }
-    ChatLib.command("pc " + receivingPlayerName + " was kicked from the party by " + formattedSenderName + ".");
+    else if (receivingPlayerName === "") {
+      return;
+    }
+    outputCommand("pc " + receivingPlayerName + " was kicked from the party by " + formattedSenderName + ".");
     waitAndOutputCommand("p remove " + receivingPlayerName, defaultTimeoutMillis);
     break;
+  case "ban":
+    // fallthrough for additional alias
   case "block":
     if (checkSetting("BingoPartyFeatures", "Party block", command) < 0)
       return;
     receivingPlayerName = commandArgument;
-    ChatLib.command("ignore add " + receivingPlayerName);
+    if (receivingPlayerName === "") {
+      return;
+    }
+    outputCommand("ignore add " + receivingPlayerName);
     waitAndOutputCommand("p remove " + receivingPlayerName, defaultTimeoutMillis+500);
     waitAndOutputCommand("pc " + receivingPlayerName + " was removed from the party and blocked from rejoining by " + formattedSenderName + ".", defaultTimeoutMillis);
     break;
+  case "unban":
+    // fallthrough for additional alias
   case "unblock":
     if (checkSetting("BingoPartyFeatures", "Party unblock", command) < 0)
       return;
     receivingPlayerName = commandArgument;
-    ChatLib.command("ignore remove " + receivingPlayerName);
+    outputCommand("ignore remove " + receivingPlayerName);
     break;
   case "open":
     // fallthrough for additional aliases
@@ -250,11 +292,11 @@ function executeHypixelPartyCommand(formattedSenderName, command, commandArgumen
       return;
     // Hypixel's lowest for a stream is a maximum of two members, but that
     // does not really make sense for the bingo party. Adapt as needed.
-    minimumPartySlots = 10; // 20, 30, …
-    maximumPartySlots = 100; // Hypixel server-given limit
+    let minimumPartySlots = 10; // 20, 30, …
+    let maximumPartySlots = 100; // Hypixel server-given limit
     // number of party slots count to open up for (rarely, if ever, has a reason
     // not to be 100). If it isn't integer, we use the maximum as default
-    newPartySize = parseInt(commandArgument);
+    let newPartySize = parseInt(commandArgument);
     if (newPartySize != NaN) {
       if (commandArgument >= minimumPartySlots && commandArgument <= maximumPartySlots) {
         // only assign user input as max. party size if it's valid, stick
@@ -262,7 +304,7 @@ function executeHypixelPartyCommand(formattedSenderName, command, commandArgumen
         maximumPartySlots = commandArgument;
       }
     }
-    ChatLib.command("pc Party size was set to " + maximumPartySlots + " by " + formattedSenderName + ".");
+    outputCommand("pc Party size was set to " + maximumPartySlots + " by " + formattedSenderName + ".");
     waitAndOutputCommand("stream open " + maximumPartySlots, defaultTimeoutMillis);
     break;
   case "inv":
@@ -271,13 +313,17 @@ function executeHypixelPartyCommand(formattedSenderName, command, commandArgumen
     if (checkSetting("BingoPartyFeatures", "Party invite", command) < 0)
       return;
     receivingPlayerName = commandArgument;
-    ChatLib.command("p invite " + receivingPlayerName);
+    if (receivingPlayerName == null || receivingPlayerName.length == 0) {
+      // no name supplied, thus invite command sender instead
+      receivingPlayerName = removeRank(formattedSenderName).toLowerCase();
+    }
+    outputCommand("p invite " + receivingPlayerName);
     waitAndOutputCommand("pc " + formattedSenderName + " invited " + receivingPlayerName + " to the party.", defaultTimeoutMillis);
     break;
   case "allinvite":
     if (checkSetting("BingoPartyFeatures", "Party allinvite", command) < 0)
       return;
-    ChatLib.command("p setting allinvite");
+    outputCommand("p setting allinvite");
     waitAndOutputCommand("pc " + formattedSenderName + " toggled allinvite setting.", defaultTimeoutMillis);
     break;
   case "say":
@@ -285,54 +331,69 @@ function executeHypixelPartyCommand(formattedSenderName, command, commandArgumen
   case "speak":
     if (checkSetting("BingoPartyFeatures", "Party speak", command) < 0)
       return;
-    // remove the first two words – "!p command" – from the message string,
-    // where command := {speak,say}
-    // TODO: keep the number of words "sliced off" UPDATED in case of
-    // ever moving away from `!p command` system, to e.g. just `!command`
-    const messageToBroadcast = message.split(" ").slice(2).join(" ");
-    ChatLib.command("pc " + formattedSenderName + ": " + messageToBroadcast);
+    outputCommand("pc " + formattedSenderName + ": " + messageToBroadcast);
+    break;
+  case "pl":
+    // fallthrough for additional alias
+  case "size":
+    // TODO: dm back "party members: xx", extracted from /pl
+    // (for checking on party even while not in it…)
+    // outputCommand("pl");
+    // memberMessage = /* more parsing…? */ "";
+    // outputCommand("r " + memberMessage);
     break;
   case "rule":
     if (checkSetting("BingoPartyFeatures", "Party rule", command) < 0)
       return;
-    ruleNumber = "";
+    let ruleNumber = "";
     // Convert iterator (map.keys()) into array to check against
     // The data itself comes from data.js, just like allowlist array
     const RuleNames = Array.from(bingoBrewersRules.keys());
     /* in this case the argument to the command should be a BingoBrewers (BB)
     rule the command user wants to cite (only to be used if the argument was
     passed on to us and exists, of course; otherwise use default of rule 1) */
-    if (messageWords.length > 2) {
+    if (message.split().length > 2) {
       ruleNumber = commandArgument;
     }
     if (!RuleNames.includes(ruleNumber)) {
-      /* yes the next lines exist to enable aliases to /ap rule number (thanks 
-      once again to ooffyy for suggesting them) and what do you mean data 
-      separation from source code? nothing to see here, this is perfectly 
-      designed and implemented data management */
-      if (ruleNumber === "splash") {
-        // alias to rule 1
-        ruleNumber = "1";
-      } else if (ruleNumber === "advertising" || ruleNumber === "ad") {
-        // aliases to rule 4
-        ruleNumber = "4";
-      } else /* default to showing rule 1 */ ruleNumber = "1";
+      // default to rule 1
+      ruleNumber = "1";
     }
-    ChatLib.command("pc --- Bingo Brewers rules ---");
+    outputCommand("pc --- Bingo Brewers rules ---");
     waitAndOutputCommand("pc Rule #" + ruleNumber + ": " + bingoBrewersRules.get(ruleNumber), defaultTimeoutMillis);
     break;
   case "help":
     if (checkSetting("BingoPartyFeatures", "!p help", command) < 0)
       return;
-      ChatLib.command("r For a list of available commands see github dot com/aphased/bingopartytools");
+    /* The use of three different messages with the same content is to prevent
+    Hypixel's blocking of repeatedly sending the same in direct messages. For
+    this, three differing messages are needed/suffice. */
+    outputCommand(helpMessages[helpOutputIndex]);
+    helpOutputIndex++;
+    /* "reset" to outputting the first message from the selection again next time the help command is used, cycling through all available messages */
+    if (helpOutputIndex === helpMessages.length) helpOutputIndex = 0;
+    break;
+  case "cmd":
+    /* Administrator-only command: will directly execute _whatever_ is received.
+    Due to this being de facto equivalent to having direct (even if chat-only)
+    access to the account, I will only let myself have this permission, since
+    BingoParty is, in fact, my account. */
+    if (casePreservingRankRemovedIGN.toLowerCase() != "aphased") {
+      waitAndOutputCommand("r Hi " + casePreservingRankRemovedIGN + ", use !p help ", 500);
+    }
+    /* This has to include the exact message, the same as it would be "typed"
+    by a player, _minus_ the preceding slash. Examples:
+    `pc hello everybody`
+    `msg IGN <message>` */
+    outputCommand(messageToBroadcast);
     break;
   default:
     /* The default case represents any non-valid command, thus we point towards
-    the usage/help command since we know the attempt was sent by a user with command/party moderation permissions. */
+    the usage/help command since we know the attempt was sent by a user with
+    command/party moderation permissions. */
     waitAndOutputCommand("r Hi " + casePreservingRankRemovedIGN + ", use !p help ", 500);
     // tmp/debug:
-    console.log("[default case activated] No party command ran");
+    // console.log("[default case activated] No party command ran");
     break;
   }
 }
-
--- data.js
+++ data.js
@@ -1,9 +1,25 @@
-/* The players listed in this allowlist here will have access to all party 
-commands in a basic, binary first version of the permission system (either all 
-perms or none)… 
-Ideally we could allow a broader range of accounts e.g. to mute the party, but 
-reserve kicking/blocking people to a smaller group  – PRs that implement this
-(or better data structures in general) are welcomed. */
+/* TODO: "properly" separate data from source code, for now it's at least moved
+into a different file. Better would be, as all the in-game names are effectively
+"settings": treat them as such or at least in some different way from the
+Discord server's rules as actual data */
+
+
+/**
+ * The current (2024-01) expected party leader's in-game account
+ * name for probably at least the next few Bingo events – not strictly a
+ * necessary requirement, but used for nicer-looking output formatting
+ */
+const partyHostNameWithoutRank = "BingoParty";
+
+
+/* The players listed in the following allowlist will have access to all
+implemented party commands in a basic, first binary version of the permission
+system – i.e., a player either has all mod permissions or none.
+Ideally we could allow a broader range of accounts e.g. to mute the party, but
+reserve blocking people to a smaller "staff" or "admin" group  – PRs that implement this (or better data structures in general) are welcomed.
+TODO: the code in bpt.js currently expects these in-game name to be in all
+lowercase, and also obviously IGN changes have to be considered/updated
+manually, which wouldn't be necessary if I worked with e.g. UUIDs? */
 /*
 Explanation: on this list there should be:
 - BingoBrewers' staff and splashers and their alt accounts
@@ -15,14 +31,21 @@ Explanation: on this list there should be:
 - Finally me (aphased) and my alt account for testing + control purposes,
   the "BingoParty" Minecraft account I made.
 */
-/* TODO: the code in bpt.js currently expects these in-game name to be in all 
-lowercase, and also obviously IGN changes have to be considered/updated 
-manually, which wouldn't be necessary if I worked with e.g. UUIDs? */
-allowlist = ["ign1", "ign2", "ign3" /* … */];
+const allowlist = ["hunterhihunter", "hub14", "indigo_polecat", "lcat_", "p0is",
+"p0is_", "bossflea", "mushedglowytonic", "sergeantsar", "baldgeant",
+"killz_stromzy", "stromzzy", "ooffyy", "bingosplasher", "batmancrtns",
+"netomnia", "watobato", "calva", "furken", "notcookies", "mafrylikebedwars",
+"notogfishyboi", "thathungrygoat", "foqh", "end_game_player", "gleolp",
+"domx2e", "karicc", "potjezout", "potjepeper", "ay212", "scathapet",
+"sakuricetwitch", "skyyblockerr", "smoothegg", "zerostulip", "dousedcharizard",
+"987654321kaboom", "katoulis", "tryp0mc", "timecharm", "dredlig", "aoik",
+"foragerredstone", "demonhunter990", "h8re", "godwyyn", "skyerzz", "citria",
+"benc1ark", "alexxoffi", "remittal", "crxmsxn", "erikeriklol",
+"kleinkolibri711", "rngecko", "aphased", "bingoparty"];
 
 
 // TODO: don't just call it data.js (move to proper data format or something)
-bingoBrewersRules = new Map([
+const bingoBrewersRules = new Map([
   //["1", "Do not ask when a splash is. This can be very annoying, it's when you get pinged in #splashes."],
   // cut original full wording due to Minecraft chat length limit, down to…:
   ["1", "Do not ask when a splash is. It's when you get pinged in #splashes."],
@@ -36,8 +59,15 @@ bingoBrewersRules = new Map([
   // ["a", "Use common sense; if something is not listed here do not treat it as a loophole. You will still be punished."],
   ["a", "Use common sense; don't argue for loopholes. You will still be punished."],
   // I took the liberty to add this
-  ["p", "Party addition: If you insult people, expect to be kicked."]
+  ["p", "Party addition: If you insult people, expect to be kicked."],
+  /* yes the next lines exist to enable aliases to /ap rule number (thanks once
+  again to ooffyy for suggesting them) and what do you mean data duplication,
+  not a single source of truth? nothing to see here in my code this is
+  perfectly designed and implemented data management */
+  ["splash", "Do not ask when a splash is. It's when you get pinged in #splashes."],
+  ["advertising", "Advertising is prohibited. This includes external splashes."],
+  ["ad", "Advertising is prohibited. This includes external splashes."],
 ]);
 
 
-export { allowlist, bingoBrewersRules }
+export { allowlist, bingoBrewersRules, partyHostNameWithoutRank }
